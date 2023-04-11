160bbb155f67d331d639f16a0096c2f69829dcf0
Sean P. Harrington <spharrin@callutheran.edu>
2023-04-11 09:36:11 -0700

    Update datetime.asm

--- src/rom/v1-3/datetime.asm
+++ src/rom/v1-3/datetime.asm
@@ -11,39 +11,38 @@
 ;
 
 
-Two Digit BCD Numbers
-
-
-
-
-DS 1244 Time Shadow Registers
-FACLO   $38E4  0 Centiseconds    
-
-
-FACMO   $38E5  1 Seconds         
-FACHO   $38E6  2 Minutes         
-FAC     $38E7  3 Hour            
-
-TI$
-FBUFFR  $38E8   Y  
-        $38E9   Y
-        $38EA   M
-        $38EB   M
-        $38EC   D
-        $38ED   D
-        $38EE   H
-        $38EF   H
-        $38F0   M
-        $38F1   M
-        $38F2   S
-        $38F3   S
-        $38F4   C
-        $38F5   C
-
-DS 1244 Date Shadow Registers
-RESHO   $38F6  5 Day             
-RESMO   $38F7  6 Month           
-RESLO   $38F8  7 Year            
+; Curtis notes:
+; -----------------------
+; Two Digit BCD Numbers
+;
+;DS 1244 Time Shadow Registers
+;FACLO   $38E4  0 Centiseconds    
+;
+;
+;FACMO   $38E5  1 Seconds         
+;FACHO   $38E6  2 Minutes         
+;FAC     $38E7  3 Hour            
+;
+;TI$
+;FBUFFR  $38E8   Y  
+;        $38E9   Y
+;        $38EA   M
+;        $38EB   M
+;        $38EC   D
+;        $38ED   D
+;        $38EE   H
+;        $38EF   H
+;        $38F0   M
+;        $38F1   M
+;        $38F2   S
+;        $38F3   S
+;        $38F4   C
+;        $38F5   C
+;
+;DS 1244 Date Shadow Registers
+;RESHO   $38F6  5 Day             
+;RESMO   $38F7  6 Month           
+;RESLO   $38F8  7 Year            
 
 ;------------------------------------------------------------------------------
 ;     DateTime Command
@@ -66,7 +65,7 @@ FN_DTM:
 ;
 ; DateTime text should begin at $3379 / 13177 (9,22) - SPH
 
-DTMSPL_LOC = $3379
+DTMSPL_STRTLOC = $3379
 
 SPL_DATETIME:
         RET
