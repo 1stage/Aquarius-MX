0e5ae76464e7fae213ccbec8a2535d8dc2322a34
Sean P. Harrington <spharrin@callutheran.edu>
2023-05-25 10:29:05 -0700

    Doc updates

--- src/rom/v2-0/extbasic.asm
+++ src/rom/v2-0/extbasic.asm
@@ -506,7 +506,7 @@ ST_COPY:
 ;;;  - SWAP < variable >, < variable >
 ;;;    - Action: Exchanges the values of two variables.
 ;;;      - The variables must be of the same type or a TM error results.
-;;; ### EXAMPLES:
+;;; ### EXAMPLE:
 ;;; ``` 
 ;;;   10 A$=" ONE " : B$=" ALL ": C$="FOR"
 ;;;   20 PRINT A$ C$ B$
@@ -569,6 +569,14 @@ MOVVFM:	                    ;MOVE VALUE FROM (HL) TO (DE)
 ;;; ### EXAMPLES:
 ;;; ` ERASE A `
 ;;; > Removes array A() from memory.
+;;;
+;;; ` 10 DIM B$(20) `
+;;;
+;;; ` 20 ERASE B$ `
+;;;
+;;; ` 30 DIM B$(10) `
+;;;
+;;; > Dimensions B$ as a 20 unit string array, then ERASES it, then redimensions it as a 10 unit array.
 ;----------------------------------------------------------------------------
 ST_ERASE:
     ld      a,1
